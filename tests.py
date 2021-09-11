902bab274bfeb1ce5883b1f76bad48e3a1664ac1
cianid <cianid@kali.kali>
2021-09-11 15:09:01 +0300

    main test

---
+++ tests.py
@@ -0,0 +1,11 @@
+import yandex_results_analyzer.selenium as Y
+import time
+
+
+if __name__ == '__main__':
+    browser = Y.get_search_results(key_request='Linux Forever', ads=True)
+    driver = browser['driver']
+    search_results = browser['search_results']
+    print("Search results:\n\r", search_results)
+    time.sleep(3)
+    driver.close()
